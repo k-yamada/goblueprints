9ca9e15d001c542894cef8a70381d07bba62dd37
k-yamada <yamadakazu45@gmail.com>
2017-02-06 01:51:39 +0900

    Add client.go and room.go

---
+++ chat/room.go
@@ -0,0 +1,12 @@
+package main
+
+type room struct {
+	// forwardは他のクライアントに転送するためのメッセージを保持するチャネルです
+	forward chan []byte
+	// joinはチャットルームに参加しようとしているクライアントのためのチャネルです
+	join chan *client
+	// leaveはチャットルームから退室しようとしているクライアントのためのチャネルです
+	leave chan *client
+	// clientsには在室している全てのクライアントが保持されます
+	clients map[*client]bool
+}
