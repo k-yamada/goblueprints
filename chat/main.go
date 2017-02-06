693d8f5c2a47c5ee188165f80d73722f987d69e1
Kazuhiro Yamada <yamadakazu45@gmail.com>
2017-02-06 20:45:15 +0900

    Add tracer

--- chat/main.go
+++ chat/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
@@ -27,6 +28,7 @@ func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
 	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	// チャットルームを開始します
